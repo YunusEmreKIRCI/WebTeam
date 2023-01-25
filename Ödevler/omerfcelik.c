f274d4c73a7804effc2813e9b231157c13912cf5
Ömer Furkan Çelik <113354329+nonTrouble@users.noreply.github.com>
2023-01-25 19:34:19 +0300

    Project has been renewed by using dma
    No need to describe

--- Ödevler/omerfcelik.c
+++ Ödevler/omerfcelik.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int main()
@@ -73,4 +73,93 @@ int main()
     printf("\n\n%d kez denk gelmis.\n\n\n",denk_gelis);
 
     return 0;
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    srand(time(0));
+
+    int **arr = (int **)calloc(10,sizeof(int *));
+    int *bir_sayilari = (int *)calloc(10,sizeof(int));
+    int *sutuns = (int *)calloc(2,sizeof(int));
+    int *indexes = (int *)calloc(2,sizeof(int));
+    int *denklik = (int *)calloc(2,sizeof(int));
+
+    for(int i = 0;i<10;i++) {
+        arr[i] = (int *)malloc(10 * sizeof(int));
+    }
+
+    for(int i = 0;i<2;i++){
+        sutuns[i] = i;
+        indexes[i] = i;
+    }
+
+
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++)   {
+            *(*(arr + i) + j) = rand()%2;
+        }
+    }
+
+    for(int i = 0;i<10;i++) {
+        denklik[1] = 0;
+        for(int j = 0;j<10;j++) {
+            if(arr[j][i]==1) {
+                denklik[1]++;
+            }
+        }
+        bir_sayilari[i] = denklik[1];
+    }
+
+    for(int i = 0;i<10;i++) {
+        for(int j = 0;j<10;j++) {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("-------------------\n");
+
+    for(int i = 0;i<10;i++) {
+        printf("%d ",bir_sayilari[i]);
+    }
+
+    for(int i = 0;i<10;i++) {
+        if(bir_sayilari[i]>=sutuns[0]) {
+            indexes[0] = i;
+            sutuns[0] = bir_sayilari[i];
+        }
+    }
+
+    for(int i = 0;i<10;i++) {
+        if(i!=indexes[0]) {
+            if(bir_sayilari[i]>=sutuns[1]) {
+                indexes[1] = i;
+                sutuns[1] = bir_sayilari[i];
+            }
+        }
+    }
+
+    printf("\n\n%d ve %d 1'in en fazla bulunma sayilari",sutuns[0],sutuns[1]);
+    printf("\n\n%d ve %d 1'in en fazla bulundugu sutunlarin indexleri\n",indexes[0],indexes[1]);
+
+    for(int i = 0;i<10;i++) {
+        if(arr[i][indexes[0]]==1 && arr[i][indexes[1]]==1) {
+            denklik[0]++;
+            printf("\n%d. satirda denk gelmis\n",i+1);
+        }
+    }
+    printf("\n\n%d kez denk gelmis.\n\n\n",denklik[0]);
+
+    free(arr);
+    free(bir_sayilari);
+    free(sutuns);
+    free(indexes);
+    free(denklik);
+
+
+    return 0;
 }
+
