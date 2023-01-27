2e156c3cec8c457707dbf2c1c541297787e98bdd
unknown <kutaypacal@hotmail.com>
2023-01-27 19:50:30 +0300

    Kutay Paçal

--- Ödevler/kutaypacal.c
+++ Ödevler/kutaypacal.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -43,12 +44,79 @@ int main()
     for(int satir=0; satir<10; satir++)
     {
         printf("\n\t%d %d \t",dizi[satir][en1],dizi[satir][en2]);
-        if(dizi[satir][en1]==dizi[satir][en2])
+        if((dizi[satir][en1]==1)&&(dizi[satir][en2])==1)
         {
             printf("%d. satirda iki sutun da 1 degerine sahiptir.",satir+1);
             sayac2++;
         }
     }
     printf("\n\n%d kez 1 degerleri denk gelmistir.\n",sayac2);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    srand(time(NULL));
+    int en1=0,en2=1,sayac2=0;
+    int* sayac = malloc(10 * sizeof(int));
+    int** dizi = (int**)malloc(10 * sizeof(int*));
+    for (int i = 0; i < 10; i++)
+        dizi[i] = (int*)malloc(10 * sizeof(int));
+    for(int satir=0; satir<10; satir++)
+    {
+        printf("\n\n");
+        for(int sutun=0; sutun<10; sutun++)
+        {
+            dizi[satir][sutun]=rand()%2;
+            printf("%d ",dizi[satir][sutun]);
+        }
+    }
+    printf("\n*******************\n");
+    for(int sutun=0; sutun<10; sutun++)
+    {
+        sayac[sutun]=0;
+        for(int satir=0; satir<10; satir++)
+        {
+            if(dizi[satir][sutun]==1)
+            {
+                sayac[sutun]++;
+            }
+        }
+    }
+    for(int sutun=0; sutun<10; sutun++)
+        printf("%d ",sayac[sutun]);
+    for(int sutun=2; sutun<10; sutun++)
+    {
+        if(sayac[en1]<sayac[sutun])
+        {
+            if(sayac[en1]<sayac[en2])
+                en1=sutun;
+            else
+                en2=sutun;
+            continue;
+        }
+    }
+    printf("\n\nEn fazla 1'i bulunduran sutunlar %d. ve %d. sutunlardir.\n\nSirasiyla %d ve %d tane 1 bulundururlar.\n\n",en1+1,en2+1,sayac[en1],sayac[en2]);
+    for(int satir=0; satir<10; satir++)
+    {
+        printf("\n\t%d %d \t",dizi[satir][en1],dizi[satir][en2]);
+        if((dizi[satir][en1]==1)&&(dizi[satir][en2])==1)
+        {
+            printf("%d. satirda iki sutun da 1 degerine sahiptir.",satir+1);
+            sayac2++;
+        }
+    }
+    printf("\n\n%d kez 1 degerleri denk gelmistir.\n",sayac2);
+    for (int i = 0; i < 10; i++)
+        free(dizi[i]);
+    free(dizi);
+    free(sayac);
+    free(en1);
+    free(en2);
+    free(sayac2);
     return 0;
 }
